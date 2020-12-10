0e9d0fc4ec5c60d100133544936b7ebe2da3a6e8
Roury Hinds <roury_hinds@outlook.com>
2020-12-10 18:40:57 +0000

    can now dock and release multiple bikes - tasting passing for raising errors when there are no bikes or dock is full - step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
-    def release_bike
-        Bike.new
-    end
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+  
+  def release_bike
+    fail 'No bikes, bro' if @bikes.empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail 'This dock be full, bro' if @bikes.count >= 20
+    @bikes << bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,48 @@
 require 'docking_station'
 
 describe DockingStation do
-    it { is_expected.to respond_to :release_bike }
-    it "can call working on docking_station.release_bike => true" do
-        bike = subject.release_bike
-        expect(bike).to be_working 
+  it { is_expected.to respond_to :release_bike }
+
+  it "can call working on docking_station.release_bike => true" do
+    subject.dock(Bike.new)
+    bike = subject.release_bike
+    expect(bike).to be_working 
+  end
+
+  it "respond to the dock method with 1 argument" do
+    is_expected.to respond_to(:dock).with(1).argument
+  end
+
+  it "respond to the bike method" do
+    is_expected.to respond_to :bikes
+  end
+  describe '#dock' do
+    it "docks a bike" do
+      bike = Bike.new
+      expect(subject.dock(bike).pop).to eq bike
     end
+
+    it 'raises an error when the dock is full and user tries to dock a bike' do
+      20.times {subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error('This dock be full, bro')
+    end
+
+  end
+  it "return docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bikes.pop).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error if there is no bike to release' do
+      expect { subject.release_bike }.to raise_error('No bikes, bro')
+    end
+  end
 end
